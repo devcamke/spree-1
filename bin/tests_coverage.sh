46434dc6c968f8c50890f38231d28d011f9b10cb
Mateusz Porębski <mateusz.porebski99@gmail.com>
2023-12-12 10:53:26 +0100

    Final version

--- bin/tests_coverage.sh
+++ bin/tests_coverage.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+
 mkdir -p tmp/
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./tmp/cc-test-reporter
 chmod +x ./tmp/cc-test-reporter
@@ -9,6 +10,7 @@ chmod +x ./tmp/cc-test-reporter
 export GIT_BRANCH="$CIRCLE_BRANCH"
 export GIT_COMMIT_SHA="$CIRCLE_SHA1"
 export GIT_COMMITTED_AT="$(date +%s)"
+
 
 ./tmp/cc-test-reporter format-coverage -t simplecov -o tmp/codeclimate.api.json /tmp/workspace/simplecov/api/.resultset.json
 ./tmp/cc-test-reporter format-coverage -t simplecov -o tmp/codeclimate.core.json /tmp/workspace/simplecov/core/.resultset.json
